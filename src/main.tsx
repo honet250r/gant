831ea1cc02dd76fedde341f2b6b33de7642cc2b0
satoshi <forzaz@live.jp>
2026-02-12 00:39:11 +0900

    chore: 起動確認ログを追加

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('App is starting...');
+console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL ? 'Defined' : 'Undefined');
+
 ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
         <App />
